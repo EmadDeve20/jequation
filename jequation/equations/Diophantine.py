525c0eae10c057e18865fcb314f87ba582aec63c
ED <emaddeve20@gmail.com>
2021-02-23 10:54:40 +0330

    change Name

---
+++ jequation/equations/Diophantine.py
@@ -0,0 +1,27 @@
+#!/bin/env python3
+
+
+
+def linear(number_a: int, number_b: int, number_c: int = 0,target: str = "") -> int:
+    """this function for linear
+
+        |ax+by=c|
+        get number of a and number of b and return anwser
+        return x,y
+    """
+
+    if target == "":
+        if number_c == 0:
+            return -number_b/number_a, -number_a/number_b
+        else:
+            return (number_c - number_b) / number_a,(-number_c + number_b) / -number_a
+    elif target == "X" or target == "x":
+        if number_c == 0:
+            return -number_b/number_a
+        else:
+            return (number_c - number_b) / number_a
+    elif target == "Y" or target == "y":
+        if number_c == 0:
+            -number_a/number_b
+        else:
+            (-number_c + number_b) / -number_a
