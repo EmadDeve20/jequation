f005b6ba392d06c5a8046885ccc9a0c25c6252d8
ED <emaddeve20@gmail.com>
2021-02-26 22:12:10 +0330

    adding demo function

--- jequation/equations/Diophantine.py
+++ jequation/equations/Diophantine.py
@@ -25,3 +25,9 @@ def linear(number_a: int, number_b: int, number_c: int = 0,target: str = "") -> 
             -number_a/number_b
         else:
             (-number_c + number_b) / -number_a
+
+def linear_with_tow(number_of_x_one: int, number_one: int, number_of_x_two: int, number_two: int):
+    """this function can solve linear equation with tow X"""
+    result = (number_of_x_one - number_of_x_two) / (number_one - number_two)
+    return result
+
