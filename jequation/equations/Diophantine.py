de30bb43dd9d6f37ec2d48d6fa9ccf99396336ea
ED <emaddeve20@gmail.com>
2021-02-27 00:46:55 +0330

    imporve doc-string

--- jequation/equations/Diophantine.py
+++ jequation/equations/Diophantine.py
@@ -1,7 +1,7 @@
 #!/bin/env python3
 
 """
-This is Demo module for solving Diophantine equations
+This is a Demo module for solving Diophantine equations
 """
 
 
@@ -9,31 +9,35 @@ def linear(number_a: int, number_b: int, number_c: int = 0, target: str = "") ->
     """this function for linear
 
         |ax+by=c|
-        get number of a and number of b and return anwser
+        get number of a and number of b and return answer
         return x,y
     """
 
-    awnser = 0
+    answer = 0
     if target in ('', 'x,y', 'X,y', 'X,Y', 'x,Y'):
         if number_c == 0:
-            awnser = -number_b/number_a, -number_a/number_b
+            answer = -number_b/number_a, -number_a/number_b
         else:
-            awnser = (number_c - number_b) / number_a, (-number_c + number_b) / -number_a
+            answer = (number_c - number_b) / number_a, (-number_c + number_b) / -number_a
     elif target in ("X", "x"):
         if number_c == 0:
-            awnser = -number_b/number_a
+            answer = -number_b/number_a
         else:
-            awnser = (number_c - number_b) / number_a
+            answer = (number_c - number_b) / number_a
     elif target in ("Y", "y"):
         if number_c == 0:
-            awnser = -number_a/number_b
+            answer = -number_a/number_b
         else:
-            awnser = (-number_c + number_b) / -number_a
+            answer = (-number_c + number_b) / -number_a
 
-    return awnser
+    return answer
 
 
 def linear_with_tow(number_of_x_one: int, number_one: int, number_of_x_two: int, number_two: int):
-    """this function can solve linear equation with tow X"""
+    """This function can solve a linear equation with two expressions
+
+       -----------------
+       |ax1+b1 = ax2+b2|
+       -----------------"""
     result = (number_of_x_one - number_of_x_two) / (number_one - number_two)
     return result
