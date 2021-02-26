85b28ed2545e53dd0ab733c17081646d374b54ee
ED <emaddeve20@gmail.com>
2021-02-27 00:40:45 +0330

    not complatly but improve code with pylint

--- jequation/equations/Diophantine.py
+++ jequation/equations/Diophantine.py
@@ -1,8 +1,11 @@
 #!/bin/env python3
 
+"""
+This is Demo module for solving Diophantine equations
+"""
 
 
-def linear(number_a: int, number_b: int, number_c: int = 0,target: str = "") -> int:
+def linear(number_a: int, number_b: int, number_c: int = 0, target: str = "") -> int:
     """this function for linear
 
         |ax+by=c|
@@ -10,24 +13,27 @@ def linear(number_a: int, number_b: int, number_c: int = 0,target: str = "") -> 
         return x,y
     """
 
-    if target == "":
+    awnser = 0
+    if target in ('', 'x,y', 'X,y', 'X,Y', 'x,Y'):
         if number_c == 0:
-            return -number_b/number_a, -number_a/number_b
+            awnser = -number_b/number_a, -number_a/number_b
         else:
-            return (number_c - number_b) / number_a,(-number_c + number_b) / -number_a
-    elif target == "X" or target == "x":
+            awnser = (number_c - number_b) / number_a, (-number_c + number_b) / -number_a
+    elif target in ("X", "x"):
         if number_c == 0:
-            return -number_b/number_a
+            awnser = -number_b/number_a
         else:
-            return (number_c - number_b) / number_a
-    elif target == "Y" or target == "y":
+            awnser = (number_c - number_b) / number_a
+    elif target in ("Y", "y"):
         if number_c == 0:
-            -number_a/number_b
+            awnser = -number_a/number_b
         else:
-            (-number_c + number_b) / -number_a
+            awnser = (-number_c + number_b) / -number_a
+
+    return awnser
+
 
 def linear_with_tow(number_of_x_one: int, number_one: int, number_of_x_two: int, number_two: int):
     """this function can solve linear equation with tow X"""
     result = (number_of_x_one - number_of_x_two) / (number_one - number_two)
     return result
-
