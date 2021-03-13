60392e3cb1fc0b5f895da372287f5a55bc418d07
ED <emaddeve20@gmail.com>
2021-03-13 20:35:49 +0330

    adding test mode

--- jequation/equations/Diophantine.py
+++ jequation/equations/Diophantine.py
@@ -4,7 +4,6 @@
 This is a Demo module for solving Diophantine equations
 """
 
-
 def linear_with_one_unknown\
     (number_a: int, number_b: int, number_c: int = 0, target: str = "") -> int:
     """this function for linear
@@ -43,3 +42,8 @@ def linear_with_tow_unknown\
        -----------------"""
     result = (number_of_x_one - number_of_x_two) / (number_one - number_two)
     return result
+
+if __name__ == "__main__":
+    from doctest import testmod
+    testmod(name="linear_with_one_unknown", verbose=True)
+    testmod(name="linear_with_tow_unknown", verbose=True)
