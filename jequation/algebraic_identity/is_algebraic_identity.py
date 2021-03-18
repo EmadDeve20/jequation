a8b1570344cc4d1cc275c0cf8f5e2b73129a59c9
ED <emaddeve20@gmail.com>
2021-03-18 19:53:43 +0330

    add a_and_b function for is_married function

--- jequation/algebraic_identity/is_algebraic_identity.py
+++ jequation/algebraic_identity/is_algebraic_identity.py
@@ -48,11 +48,33 @@ def is_Twosentence_square(phrase: str) -> bool:
 
 def is_married(phrase: str) -> bool:
     """this function for check your phrase is married or not"""
+    # (a + b)(b - a) and and any more like this is True for this function 
+    def a_and_b(phrase: str) -> bool:
+        # phrase = "(a+b)(a-b)"
+        phrase = phrase.replace("(", "")
+        # pharse = "a+b)a-b)"
+        phrase = phrase.replace(")", "")
+        # pharse = "a+ba-b"
+        phrase = phrase.replace("+", " ")
+        # pharse = "a ba-b"
+        phrase = phrase.replace("-", " ")
+        # pharse = "a ba b"
+        phrase = phrase.split(" ")
+        # pharse = "[a, ba, b]"
+        
+        if (phrase[0] != phrase[2]) and phrase[1] == phrase[2] + phrase[0]:
+            return True
+        if (phrase[0] == phrase[2]) and \
+            (phrase[1][0:int(len(phrase[1])/2)] == \
+                phrase[1][int(len(phrase[1])/2):len(phrase[1])]):
+            return True
+        return False
+
     check = re.match(r"\(.+-.+\)\(.+\+.+\)", phrase)
-    if check != None and check.end() == len(phrase):
+    if check != None and check.end() == len(phrase) and a_and_b(phrase):
         return True
     check = re.match(r"\(.+\+.+\)\(.+-.+\)", phrase)
-    if check != None and  check.end() == len(phrase):
+    if check != None and  check.end() == len(phrase) and a_and_b(phrase):
         return True
     return False
 
