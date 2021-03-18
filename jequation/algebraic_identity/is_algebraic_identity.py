24247b25f7520982b939792cfb09676c3f51100c
ED <emaddeve20@gmail.com>
2021-03-18 20:00:45 +0330

    improve code with use pylint :D

--- jequation/algebraic_identity/is_algebraic_identity.py
+++ jequation/algebraic_identity/is_algebraic_identity.py
@@ -7,22 +7,21 @@ I used the Persian names of alliances for all functions
 import re
 
 
-def is_Twosentence_square(phrase: str) -> bool:
+def is_twosentence_square(phrase: str) -> bool:
     """this function for check your phrase is binomial or not"""
-    # (a -|+ b)(b -|+ a) and and any more like this is True for this function 
+    # (a -|+ b)(b -|+ a) and and any more like this is True for this function
     def a_and_b(phrase: str) -> bool:
         # phrase = "(a+b)(a+b)"
         phrase = phrase.replace("(", "")
         # pharse = "a+b)a+b)"
         phrase = phrase.replace(")", "")
         # pharse = "a+ba+b"
-        
         if phrase.find("+") != -1:
             phrase = phrase.split("+")
         elif phrase.find("-") != -1:
             phrase = phrase.split("-")
         # pharse = "[a, ba, b]"
-        
+
         if (phrase[0] != phrase[2]) and phrase[1] == phrase[2] + phrase[0]:
             return True
         if (phrase[0] == phrase[2]) and \
@@ -43,12 +42,12 @@ def is_Twosentence_square(phrase: str) -> bool:
         return True
     check = re.match(r"\(.+-.+\)\^2", phrase)
     if check != None and check.end == len(phrase):
-            return True
+        return True
     return False
 
 def is_married(phrase: str) -> bool:
     """this function for check your phrase is married or not"""
-    # (a + b)(b - a) and and any more like this is True for this function 
+    # (a + b)(b - a) and and any more like this is True for this function
     def a_and_b(phrase: str) -> bool:
         # phrase = "(a+b)(a-b)"
         phrase = phrase.replace("(", "")
@@ -61,7 +60,7 @@ def is_married(phrase: str) -> bool:
         # pharse = "a ba b"
         phrase = phrase.split(" ")
         # pharse = "[a, ba, b]"
-        
+
         if (phrase[0] != phrase[2]) and phrase[1] == phrase[2] + phrase[0]:
             return True
         if (phrase[0] == phrase[2]) and \
@@ -78,19 +77,19 @@ def is_married(phrase: str) -> bool:
         return True
     return False
 
-def is_Threesentence_square(phrase: str) -> bool:
+def is_threesentence_square(phrase: str) -> bool:
     pass
 
-def is_Twosentence_cube(phrase: str) -> bool:
+def is_twosentence_cube(phrase: str) -> bool:
     pass
 
 def is_fat_and_thin(phrase: str) ->bool:
     pass
 
-def is_Common_sentence(phrase: str) -> bool:
+def is_common_sentence(phrase: str) -> bool:
     pass
 
-def is_Newtons_binomial_expansion_sentence(phrase: str) -> bool:
+def is_newtons_binomial_expansion_sentence(phrase: str) -> bool:
     pass
 
 def is_lagrange_alliance(phrase: str) -> bool:
