d6a1fd172768e854e5e5a32ae26140e22d4c0383
ED <emaddeve20@gmail.com>
2021-05-07 21:39:01 +0430

    so i remove the fucking print`s for debug. so sorry for this fking commit :|

--- jequation/algebraic_identity/is_algebraic_identity.py
+++ jequation/algebraic_identity/is_algebraic_identity.py
@@ -210,7 +210,6 @@ def is_fat_thin(phrase: str) -> bool:
             for i in range(2, 5):
                 dynamic_number = float(phrase[i])
                 if static_naumber ** 2 == dynamic_number:
-                    print("212")
                     score += 1
                     break
         except ValueError:
@@ -221,7 +220,6 @@ def is_fat_thin(phrase: str) -> bool:
                 if dynamic_number.find("^2"):
                     dynamic_number = dynamic_number[:dynamic_number.find("^2")]
                 if static_naumber == dynamic_number:
-                    print("222")
                     score += 1
                     break
 
@@ -231,7 +229,6 @@ def is_fat_thin(phrase: str) -> bool:
             for i in range(2, 5):
                 dynamic_number = float(phrase[i])
                 if static_naumber ** 2 == dynamic_number:
-                    print("231")
                     score += 1
                     break
         except ValueError:
@@ -242,7 +239,6 @@ def is_fat_thin(phrase: str) -> bool:
                 if dynamic_number.find("^2"):
                     dynamic_number = dynamic_number[:dynamic_number.find("^2")]
                 if static_naumber == dynamic_number:
-                    print("241")
                     score += 1
                     break
 
@@ -251,7 +247,6 @@ def is_fat_thin(phrase: str) -> bool:
             for i in range(2, 5):
                 try:
                     if float(phrase[i]) == match_number:
-                        print("248")
                         score += 1
                         break
                 except ValueError:
@@ -261,7 +256,6 @@ def is_fat_thin(phrase: str) -> bool:
             match_number = phrase[0] + phrase[1]
             for i in range(2, 5):
                 if len(phrase[i]) == len(match_number):
-                    print("254")
                     score += 1
                     break
 
